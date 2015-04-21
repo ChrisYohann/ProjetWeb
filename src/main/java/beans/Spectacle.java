9e6605cf298f2bef23b2b4a91db4920f55b1c566
MatyldeIgier <matyldeigier@gmail.com>
2015-04-21 21:32:48 +0200

    beans

---
+++ src/main/java/beans/Spectacle.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans;
+
+import java.awt.Image;
+
+/**
+ *
+ * @author igierm
+ */
+public class Spectacle {
+    private String  name;
+    private int numero;
+    private String description;
+    private int numSalle;
+    private Image affiche;
+    
+    public String getName() {
+        return this.name;
+    }
+    public int getNumero() {
+        return this.numero;
+    }
+    public String getDescription() {
+        return this.description;
+    }
+    public int getNumSalle() {
+        return this.numSalle;
+    }
+    public Image getAffiche() {
+        return this.affiche;
+    }
+    
+    public void setName(String name) {
+        this.name=name;
+    }
+    
+    public void setNumero(int numero) {
+        this.numero=numero;
+    }
+    
+    public void setDescription(String description) {
+        this.description=description;
+    }
+    
+    public void setNumSalle(int numSalle) {
+        this.numSalle=numSalle;
+    }
+    
+    public void setAffiche(Image affiche) {
+        this.affiche=affiche;
+    }
+    
+    
+   
+}
