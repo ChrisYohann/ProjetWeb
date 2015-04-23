c8b1fefc2ae937d4f84ebba02fd4546a3631f294
MatyldeIgier <matyldeigier@gmail.com>
2015-04-23 09:48:30 +0200

    reset

---
+++ Reset.sql
@@ -0,0 +1,48 @@
+DROP TABLE reservation;
+
+DROP TABLE representation;
+
+DROP TABLE achat;
+
+DROP TABLE place;
+
+DROP TABLE rang;
+
+DROP TABLE ticket;
+
+DROP TABLE categorie;
+
+DROP TABLE dossier;
+
+DROP TABLE programmeur;
+
+DROP TABLE utilisateur;
+
+DROP TABLE salle;
+
+DROP TABLE dateT;
+
+DROP TABLE spectacle;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
