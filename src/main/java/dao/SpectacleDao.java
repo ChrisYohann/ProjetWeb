5cbae9c4e4f3ee197d71f7fc0571e1c50eb1f344
Yohann <i.nhyne@gmail.com>
2015-04-23 21:16:29 +0200

    aaaaa

---
+++ src/main/java/dao/SpectacleDao.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import beans.Spectacle;
+
+/**
+ *
+ * @author chris
+ */
+public interface SpectacleDao {
+    
+    public void creer(Spectacle spectacle) throws DAOException ;
+    
+    public Spectacle trouver(int numSpect) throws DAOException ;
+    
+}
