356669b8730a4ad026b975ad1413cec7f201c39a
MatyldeIgier <matyldeigier@gmail.com>
2015-04-23 09:50:09 +0200

    insert

---
+++ Insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO utilisateur (login, nomUt, prenomUt,AdresseUt,mdpUt) VALUES ('root', 'root',  'root', '', 'root');
+INSERT INTO utilisateur (login, nomUt, prenomUt,AdresseUt,mdpUt) VALUES ('yohann', 'hako',  'root', '', 'root');
