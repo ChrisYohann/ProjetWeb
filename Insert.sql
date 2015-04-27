319856f6ee9cb2b4322c8ffd1193f77632d20b2a
pasmoi75@github.com <celesti.jmarc@gmail.com>
2015-04-27 14:42:33 +0200

    yo

--- Insert.sql
+++ Insert.sql
@@ -1,3 +1,4 @@
 INSERT INTO utilisateur (login, nomUt, prenomUt,AdresseUt,mdpUt) VALUES ('root', 'root',  'root', '', 'root');
 INSERT INTO utilisateur (login, nomUt, prenomUt,AdresseUt,mdpUt) VALUES ('yohann', 'hako',  'root', '', 'root');
 INSERT INTO programmeur (login, nomUt, prenomUt,AdresseUt,mdpUt) VALUES ('root', 'root',  'root', '', 'root');
+INSERT INTO spectacle (numSpect , nomSpect , nbrPlace ) VALUES ('1','romeo','90');
